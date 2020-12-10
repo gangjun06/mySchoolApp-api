697b513c2a4b6bc9c263f67a5acad847a74239cc
gangjun06 <me@gangjun.dev>
2020-12-10 22:59:43 +0900

    clean project again

--- models/claims.go
+++
@@ -1,8 +0,0 @@
-package models
-
-import "github.com/dgrijalva/jwt-go"
-
-type Claims struct {
-	ID uint
-	jwt.StandardClaims
-}
--- routes/v1/index.go
+++
@@ -1,7 +0,0 @@
-package v1
-
-import "github.com/gin-gonic/gin"
-
-func InitRoutes(r *gin.RouterGroup) {
-
-}
